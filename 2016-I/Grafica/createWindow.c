3c7680b5a0381ae45ff6483d6a6d6dff1bdb349c
BlackNails <rigelsile@gmail.com>
2016-06-05 09:20:10 +0900

    Create a window in OpenGL

---
+++ 2016-I/Grafica/createWindow.c
@@ -0,0 +1,13 @@
+#include <GL/glut.h>
+
+int main(int argc, char** argv) {
+glutInit(&argc, argv); 
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+glutInitWindowSize(400, 300); 
+glutInitWindowPosition(0, 0); 
+glutCreateWindow(argv[0]); 
+myInit(); 
+glutMainLoop(); 
+return 0; 
+}
+
