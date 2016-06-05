11e26d5a2f0ea001c11ea40f22f51c2e163b4837
BlackNails <rigelsile@gmail.com>
2016-06-05 09:29:34 +0900

    Update createWindow.c

--- 2016-I/Grafica/createWindow.c
+++ 2016-I/Grafica/createWindow.c
@@ -1,6 +1,10 @@
 #include <GL/glut.h>
 
+void myInit(){
+  
+}
 int main(int argc, char** argv) {
+  
 glutInit(&argc, argv); 
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 glutInitWindowSize(400, 300); 
@@ -8,6 +12,8 @@ glutInitWindowPosition(0, 0);
 glutCreateWindow(argv[0]); 
 myInit(); 
 glutMainLoop(); 
+
 return 0; 
+
 }
 
